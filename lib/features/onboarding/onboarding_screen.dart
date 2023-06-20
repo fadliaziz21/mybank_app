365656658354c8096a95eaf65f20d610356af13f
Fadli Abdul Aziz <fadli.aziz@anabatic.com>
2023-06-20 15:57:08 +0700

    add GetStarted btn and method for move to homepage

--- lib/features/onboarding/onboarding_screen.dart
+++ lib/features/onboarding/onboarding_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:mybank_app/constans/colors.dart';
 import 'package:mybank_app/features/onboarding/onboarding_data.dart';
+import 'package:mybank_app/main.dart';
 import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
 class OnBoardingPage extends StatefulWidget {
@@ -11,6 +13,7 @@ class OnBoardingPage extends StatefulWidget {
 
 class _OnBoardingPageState extends State<OnBoardingPage> {
   final controller = PageController();
+  bool isLastPage = false;
 
   @override
   void dispose() {
@@ -27,7 +30,7 @@ class _OnBoardingPageState extends State<OnBoardingPage> {
   }) {
     return Container(
       color: color,
-      padding: EdgeInsets.all(25),
+      padding: EdgeInsets.all(25.0),
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
@@ -36,23 +39,22 @@ class _OnBoardingPageState extends State<OnBoardingPage> {
             fit: BoxFit.cover,
             width: double.infinity,
           ),
-          const SizedBox(height: 18),
+          const SizedBox(height: 18.0),
           Text(
             title,
             style: TextStyle(
-              color: Colors.blue,
+              color: AppColors.mainColor,
               fontSize: 28,
               fontWeight: FontWeight.bold,
             ),
           ),
-          const SizedBox(height: 10),
+          const SizedBox(height: 10.0),
           Text(
             subtitle,
             textAlign: TextAlign.center,
             style: TextStyle(
               color: Colors.black,
               fontSize: 16,
-              // fontWeight: FontWeight.bold,
             ),
           ),
         ],
@@ -65,9 +67,14 @@ class _OnBoardingPageState extends State<OnBoardingPage> {
     return Scaffold(
       body: SafeArea(
         child: Container(
-          padding: EdgeInsets.only(bottom: 60),
+          padding: EdgeInsets.only(bottom: 60.0),
           child: PageView.builder(
             controller: controller,
+            onPageChanged: (index) {
+              setState(() {
+                isLastPage = index == onboardingList.length - 1;
+              });
+            },
             itemCount: onboardingList.length,
             itemBuilder: (context, index) {
               return buildPage(
@@ -80,51 +87,95 @@ class _OnBoardingPageState extends State<OnBoardingPage> {
           ),
         ),
       ),
-      bottomSheet: Container(
-        padding: EdgeInsets.symmetric(horizontal: 15.00),
-        height: 80,
-        color: Colors.white,
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            TextButton(
-              onPressed: () {
-                controller.jumpToPage(onboardingList.length - 1);
-              },
-              child: Text('Skip'),
-            ),
-            Center(
-              child: SmoothPageIndicator(
-                controller: controller,
-                count: onboardingList.length,
-                // effect: WormEffect(
-                //   spacing: 16,
-                //   dotColor: Colors.black26,
-                //   activeDotColor: Colors.teal.shade700,
-                // ),
-                onDotClicked: (index) {
-                  controller.animateToPage(
-                    index,
-                    duration: Duration(milliseconds: 500),
-                    curve: Curves.easeIn,
+      bottomSheet: isLastPage
+          ? Container(
+              height: 50.0,
+              width: 250.0,
+              margin: EdgeInsets.only(
+                bottom: 50.0,
+              ),
+              child: TextButton(
+                style: ButtonStyle(
+                  backgroundColor:
+                      MaterialStateProperty.all<Color>(AppColors.mainColor),
+                  shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                    RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(25.0),
+                      // side: BorderSide(color: AppColors.mainColor),
+                    ),
+                  ),
+                ),
+                onPressed: () async {
+                  Navigator.of(context).pushReplacement(
+                    MaterialPageRoute(
+                        builder: (context) => MyHomePage(
+                              title: 'Home Pages',
+                            )),
                   );
                 },
+                child: Text(
+                  'Get Started',
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+            )
+          : Container(
+              padding: EdgeInsets.symmetric(horizontal: 15.0),
+              height: 80.0,
+              color: Colors.white,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  TextButton(
+                    onPressed: () {
+                      controller.jumpToPage(onboardingList.length - 1);
+                    },
+                    child: Text(
+                      'Skip',
+                      style: TextStyle(
+                        color: AppColors.mainColor,
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  Center(
+                    child: SmoothPageIndicator(
+                      controller: controller,
+                      count: onboardingList.length,
+                      onDotClicked: (index) {
+                        controller.animateToPage(
+                          index,
+                          duration: Duration(milliseconds: 500),
+                          curve: Curves.easeIn,
+                        );
+                      },
+                      effect: SlideEffect(
+                        activeDotColor: AppColors.mainColor,
+                      ),
+                    ),
+                  ),
+                  TextButton(
+                    onPressed: () {
+                      controller.nextPage(
+                        duration: Duration(
+                          milliseconds: 500,
+                        ),
+                        curve: Curves.easeInOut,
+                      );
+                    },
+                    child: Text(
+                      'Next',
+                      style: TextStyle(
+                        color: AppColors.mainColor,
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ],
               ),
             ),
-            TextButton(
-              onPressed: () {
-                controller.nextPage(
-                  duration: Duration(
-                    milliseconds: 500,
-                  ),
-                  curve: Curves.easeInOut,
-                );
-              },
-              child: Text('Next'),
-            ),
-          ],
-        ),
-      ),
     );
   }
 }
