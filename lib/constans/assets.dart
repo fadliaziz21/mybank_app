aaa4a151886dc02312272c2f317f5c3ce07042bd
Fadli Abdul Aziz <fadli.aziz@anabatic.com>
2023-06-20 15:38:53 +0700

    new class for listing assets

---
+++ lib/constans/assets.dart
@@ -0,0 +1,9 @@
+class Assets {
+  static final String appLogo = 'assets/icons/mybank_icon-white.png';
+
+  static const Map<int, dynamic> onboardingImg = {
+    1: 'assets/images/onboarding/mybank_onboarding-1.png',
+    2: 'assets/images/onboarding/mybank_onboarding-2.png',
+    3: 'assets/images/onboarding/mybank_onboarding-3.png',
+  };
+}
