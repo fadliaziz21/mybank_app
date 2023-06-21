dedd09ae9f59b528f8a1c2c4a7c04fdd753aa91d
Fadli Abdul Aziz <fadli.aziz@anabatic.com>
2023-06-21 12:15:27 +0700

    change from final to const

--- lib/constans/assets.dart
+++ lib/constans/assets.dart
@@ -1,5 +1,5 @@
 class Assets {
-  static final String appLogo = 'assets/icons/mybank_icon-white.png';
+  static const String appLogo = 'assets/icons/mybank_icon-white.png';
 
   static const Map<int, dynamic> onboardingImg = {
     1: 'assets/images/onboarding/mybank_onboarding-1.png',
--- lib/constans/colors.dart
+++ lib/constans/colors.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  static final Color mainColor = Color.fromARGB(255, 0, 127, 191);
+  static const Color mainColor = Color.fromARGB(255, 0, 127, 191);
 }
