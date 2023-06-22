70f7eb6c0b035600898aae9657727b64d5f94422
Fadli Abdul Aziz <fadli.aziz@anabatic.com>
2023-06-22 15:13:06 +0700

    mdf PinInput,PinKeyNum and add PinKeyIcon wdgt

--- lib/features/pin/pin_widget.dart
+++ lib/features/pin/pin_widget.dart
@@ -13,6 +13,7 @@ class PinInput extends StatelessWidget {
         controller: textEditingController,
         enabled: false,
         obscureText: true,
+        obscuringCharacter: '●',
         textAlign: TextAlign.center,
         decoration: InputDecoration(
           contentPadding: EdgeInsets.all(15.0),
@@ -23,6 +24,7 @@ class PinInput extends StatelessWidget {
           filled: true,
           fillColor: Colors.white,
         ),
+        style: Styles.pinInputText,
       ),
     );
   }
@@ -36,15 +38,15 @@ class PinKeyNum extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: 60.0,
       height: 60.0,
+      width: 60.0,
       decoration: BoxDecoration(
         shape: BoxShape.circle,
-        color: Colors.white,
+        color: Colors.white24,
       ),
       alignment: Alignment.center,
       child: MaterialButton(
-        height: 90.0,
+        height: 60.0,
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(60.0),
         ),
@@ -53,6 +55,36 @@ class PinKeyNum extends StatelessWidget {
           '$keyNum',
           textAlign: TextAlign.center,
           style: Styles.pinKeyNumText,
+        ),
+      ),
+    );
+  }
+}
+
+class PinKeyIcon extends StatelessWidget {
+  final IconData iconName;
+  final Function() onPressed;
+  const PinKeyIcon({required this.iconName, required this.onPressed});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 60.0,
+      width: 60.0,
+      decoration: BoxDecoration(
+        shape: BoxShape.circle,
+        color: Colors.white24,
+      ),
+      alignment: Alignment.center,
+      child: MaterialButton(
+        height: 60.0,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(60.0),
+        ),
+        onPressed: onPressed,
+        child: Icon(
+          iconName,
+          color: Colors.white,
         ),
       ),
     );
