71609cbf881a22239bb7e5afc6104032391f6716
Fadli Abdul Aziz <fadli.aziz@anabatic.com>
2023-06-22 15:14:58 +0700

    add,implement pinIndexSetup,setPin,clearPin func

--- lib/features/pin/pin_screen.dart
+++ lib/features/pin/pin_screen.dart
@@ -71,17 +71,63 @@ class _PinPageState extends State<PinPage> {
                     Row(
                       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                       children: [
-                        PinKeyNum(
-                          keyNum: 1,
-                          onPressed: () {},
+                        for (int i = 1; i <= 3; i++) ...[
+                          PinKeyNum(
+                            keyNum: i,
+                            onPressed: () {
+                              pinIndexSetup(i.toString());
+                            },
+                          ),
+                        ],
+                      ],
+                    ),
+                    Styles.lineBreak25,
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        for (int i = 4; i <= 6; i++) ...[
+                          PinKeyNum(
+                            keyNum: i,
+                            onPressed: () {
+                              pinIndexSetup(i.toString());
+                            },
+                          ),
+                        ],
+                      ],
+                    ),
+                    Styles.lineBreak25,
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        for (int i = 7; i <= 9; i++) ...[
+                          PinKeyNum(
+                            keyNum: i,
+                            onPressed: () {
+                              pinIndexSetup(i.toString());
+                            },
+                          ),
+                        ],
+                      ],
+                    ),
+                    Styles.lineBreak25,
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Container(
+                          height: 60.0,
+                          width: 60.0,
                         ),
                         PinKeyNum(
-                          keyNum: 2,
-                          onPressed: () {},
+                          keyNum: 0,
+                          onPressed: () {
+                            pinIndexSetup('0');
+                          },
                         ),
-                        PinKeyNum(
-                          keyNum: 3,
-                          onPressed: () {},
+                        PinKeyIcon(
+                          iconName: Icons.backspace,
+                          onPressed: () {
+                            clearPin();
+                          },
                         ),
                       ],
                     ),
@@ -93,5 +139,63 @@ class _PinPageState extends State<PinPage> {
         ),
       ),
     );
+  }
+
+  pinIndexSetup(String pin) {
+    String strPin = '';
+
+    if (pinIndex == 0) {
+      pinIndex = 1;
+    } else if (pinIndex < 6) {
+      pinIndex++;
+    }
+
+    setPin(pinIndex, pin);
+    currentPin[pinIndex - 1] = pin;
+
+    currentPin.forEach((element) {
+      strPin += element;
+    });
+
+    if (pinIndex == 6) {
+      print(strPin);
+    }
+  }
+
+  setPin(int n, String pin) {
+    switch (n) {
+      case 1:
+        pinOneCtrlr.text = pin;
+        break;
+      case 2:
+        pinTwoCtrlr.text = pin;
+        break;
+      case 3:
+        pinThreeCtrlr.text = pin;
+        break;
+      case 4:
+        pinFourCtrlr.text = pin;
+        break;
+      case 5:
+        pinFiveCtrlr.text = pin;
+        break;
+      case 6:
+        pinSixCtrlr.text = pin;
+        break;
+    }
+  }
+
+  clearPin() {
+    if (pinIndex == 0) {
+      pinIndex = 1;
+    } else if (pinIndex == 6) {
+      setPin(pinIndex, '');
+      currentPin[pinIndex - 1] = '';
+      pinIndex--;
+    } else {
+      setPin(pinIndex, '');
+      currentPin[pinIndex - 1] = '';
+      pinIndex--;
+    }
   }
 }
