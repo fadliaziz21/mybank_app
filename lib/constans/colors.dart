dd6c8e75c8e22a8899442950a12b47eb049cacdb
Fadli Abdul Aziz <fadli.aziz@anabatic.com>
2023-06-23 10:26:00 +0700

    rename to primaryColor and add dangerColor

--- lib/constans/colors.dart
+++ lib/constans/colors.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  static const Color mainColor = Color.fromARGB(255, 0, 127, 191);
+  static const Color primaryColor = Color.fromARGB(255, 0, 127, 191);
+  static const Color dangerColor = Color.fromARGB(255, 248, 68, 79);
 }
