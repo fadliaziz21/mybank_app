75f9466670a2803352263545713361335a49fe59
Fadli Abdul Aziz <fadli.aziz@anabatic.com>
2023-06-28 16:28:01 +0700

    init transaction history scr

---
+++ lib/features/transaction_history/transaction_history_screen.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class TransactionHistoryPage extends StatefulWidget {
+  const TransactionHistoryPage({super.key});
+
+  @override
+  State<TransactionHistoryPage> createState() => _TransactionHistoryPageState();
+}
+
+class _TransactionHistoryPageState extends State<TransactionHistoryPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Transaction History'),
+      ),
+      body: Center(
+        child: Text("Transaction History"),
+      ),
+    );
+  }
+}
