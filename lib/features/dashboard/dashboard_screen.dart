65a26f22dffd161d6f5387a7f3f1822053a367aa
Fadli Abdul Aziz <fadli.aziz@anabatic.com>
2023-06-28 13:28:34 +0700

    move to _buildAppBar and _buildBottomNavigationBar

--- lib/features/dashboard/dashboard_screen.dart
+++ lib/features/dashboard/dashboard_screen.dart
@@ -16,89 +16,36 @@ class _DashboardPageState extends State<DashboardPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: AppColors.primaryColor,
-        shadowColor: Colors.black,
-        toolbarHeight: 80,
-        title: Container(
-          width: 40,
-          child: Image.asset(Assets.myBankIcon),
-        ),
-        actions: [
-          IconButton(
-            icon: Icon(
-              Icons.notifications,
-              color: Colors.white,
-              size: 30,
-            ),
-            onPressed: () {},
-          ),
-        ],
+      appBar: PreferredSize(
+        child: _buildAppBar(),
+        preferredSize: Size.fromHeight(80.0),
       ),
       body: SafeArea(
         child: _buildBody(),
       ),
-      bottomNavigationBar: Container(
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.only(
-            topLeft: Radius.circular(25),
-            topRight: Radius.circular(25),
-          ),
-          boxShadow: [
-            BoxShadow(
-              color: Colors.grey.shade800,
-              spreadRadius: 1,
-              blurRadius: 10,
-            ),
-          ],
-        ),
-        child: ClipRRect(
-          borderRadius: BorderRadius.only(
-            topLeft: Radius.circular(25.0),
-            topRight: Radius.circular(25.0),
-          ),
-          child: BottomNavigationBar(
-            currentIndex: selectedIndex,
-            onTap: (value) {
-              setState(() {
-                selectedIndex = value;
-              });
-            },
-            items: [
-              BottomNavigationBarItem(
-                icon: ImageIcon(
-                  AssetImage(dashboardFooterList[0].urlImage),
-                  size: 35,
-                ),
-                label: dashboardFooterList[0].labelName,
-              ),
-              BottomNavigationBarItem(
-                icon: ImageIcon(
-                  AssetImage(dashboardFooterList[1].urlImage),
-                  size: 35,
-                ),
-                label: dashboardFooterList[1].labelName,
-              ),
-              BottomNavigationBarItem(
-                icon: ImageIcon(
-                  AssetImage(dashboardFooterList[2].urlImage),
-                  size: 35,
-                ),
-                label: dashboardFooterList[2].labelName,
-              ),
-              BottomNavigationBarItem(
-                icon: ImageIcon(
-                  AssetImage(dashboardFooterList[3].urlImage),
-                  size: 35,
-                ),
-                label: dashboardFooterList[3].labelName,
-              ),
-            ],
-            unselectedItemColor: Colors.grey.shade600,
-            selectedItemColor: AppColors.primaryColor,
-          ),
-        ),
+      bottomNavigationBar: _buildBottomNavigationBar(),
+    );
+  }
+
+  Widget _buildAppBar() {
+    return AppBar(
+      backgroundColor: AppColors.primaryColor,
+      shadowColor: Colors.black,
+      toolbarHeight: 80.0,
+      title: Container(
+        width: 40.0,
+        child: Image.asset(Assets.myBankIcon),
       ),
+      actions: [
+        IconButton(
+          icon: Icon(
+            Icons.notifications,
+            color: Colors.white,
+            size: 30.0,
+          ),
+          onPressed: () {},
+        ),
+      ],
     );
   }
 
@@ -113,5 +60,69 @@ class _DashboardPageState extends State<DashboardPage> {
       default:
         return const Center(child: Text('Default'));
     }
+  }
+
+  Widget _buildBottomNavigationBar() {
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(25.0),
+          topRight: Radius.circular(25.0),
+        ),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.shade800,
+            spreadRadius: 1,
+            blurRadius: 10,
+          ),
+        ],
+      ),
+      child: ClipRRect(
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(25.0),
+          topRight: Radius.circular(25.0),
+        ),
+        child: BottomNavigationBar(
+          currentIndex: selectedIndex,
+          onTap: (value) {
+            setState(() {
+              selectedIndex = value;
+            });
+          },
+          items: [
+            BottomNavigationBarItem(
+              icon: ImageIcon(
+                AssetImage(dashboardFooterList[0].urlImage),
+                size: 35,
+              ),
+              label: dashboardFooterList[0].labelName,
+            ),
+            BottomNavigationBarItem(
+              icon: ImageIcon(
+                AssetImage(dashboardFooterList[1].urlImage),
+                size: 35,
+              ),
+              label: dashboardFooterList[1].labelName,
+            ),
+            BottomNavigationBarItem(
+              icon: ImageIcon(
+                AssetImage(dashboardFooterList[2].urlImage),
+                size: 35,
+              ),
+              label: dashboardFooterList[2].labelName,
+            ),
+            BottomNavigationBarItem(
+              icon: ImageIcon(
+                AssetImage(dashboardFooterList[3].urlImage),
+                size: 35,
+              ),
+              label: dashboardFooterList[3].labelName,
+            ),
+          ],
+          unselectedItemColor: Colors.grey.shade600,
+          selectedItemColor: AppColors.primaryColor,
+        ),
+      ),
+    );
   }
 }
