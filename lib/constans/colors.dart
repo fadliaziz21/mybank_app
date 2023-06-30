834f8a4134e96ab9dc6587d526155fbffa69d8ff
Fadli Abdul Aziz <fadli.aziz@anabatic.com>
2023-06-30 15:18:40 +0700

    add secondaryColor color

--- lib/constans/colors.dart
+++ lib/constans/colors.dart
@@ -3,4 +3,5 @@ import 'package:flutter/material.dart';
 class AppColors {
   static const Color primaryColor = Color.fromARGB(255, 0, 127, 191);
   static const Color dangerColor = Color.fromARGB(255, 248, 68, 79);
+  static const Color secondaryColor = Color.fromARGB(255, 66, 66, 66);
 }
