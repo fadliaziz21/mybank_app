fe5d0c9518e4e858754fae03403ceadfd6880e53
Fadli Abdul Aziz <fadli.aziz@anabatic.com>
2023-07-05 11:47:22 +0700

    add darkColor,whiteColor,whiteTransparentColor

--- lib/constans/colors.dart
+++ lib/constans/colors.dart
@@ -3,5 +3,8 @@ import 'package:flutter/material.dart';
 class AppColors {
   static const Color primaryColor = Color.fromARGB(255, 0, 127, 191);
   static const Color dangerColor = Color.fromARGB(255, 248, 68, 79);
+  static const Color darkColor = Color.fromARGB(255, 0, 0, 0);
   static const Color secondaryColor = Color.fromARGB(255, 66, 66, 66);
+  static const Color whiteColor = Color.fromARGB(255, 255, 255, 255);
+  static const Color whiteTransparentColor = Color.fromARGB(61, 255, 255, 255);
 }
