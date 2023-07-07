9007e55069f54c1cb80ce7ae411265ad56dda20c
Fadli Abdul Aziz <fadli.aziz@anabatic.com>
2023-07-07 11:02:58 +0700

    add new TransactionList class

--- lib/features/transaction_history/transaction_history_widget.dart
+++ lib/features/transaction_history/transaction_history_widget.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:mybank_app/constans/colors.dart';
+import 'package:mybank_app/constans/styles.dart';
 
 class TransactionHistoryTabBar extends StatelessWidget {
   final String labelName;
@@ -19,6 +20,82 @@ class TransactionHistoryTabBar extends StatelessWidget {
         child: Align(
           alignment: Alignment.center,
           child: Text(labelName),
+        ),
+      ),
+    );
+  }
+}
+
+class TransactionList extends StatelessWidget {
+  final String urlImage;
+  final String transactionType;
+  final String transactionName;
+  final String transactionDate;
+  final String transactionAmount;
+  const TransactionList({
+    super.key,
+    required this.urlImage,
+    required this.transactionType,
+    required this.transactionName,
+    required this.transactionDate,
+    required this.transactionAmount,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: Styles.eiHorizontal20Vertical10,
+      child: Container(
+        height: 75.0,
+        decoration: BoxDecoration(
+          color: AppColors.whiteColor,
+          borderRadius: Styles.borderRadCircular10,
+          boxShadow: const [
+            Styles.boxShadowSmall,
+          ],
+        ),
+        child: Row(
+          children: [
+            Padding(
+              padding: Styles.eiAll7,
+              child: Image(
+                image: AssetImage(
+                  urlImage,
+                ),
+                height: 75.0,
+              ),
+            ),
+            Padding(
+              padding: Styles.eiAll7,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    transactionName,
+                    textAlign: TextAlign.left,
+                    style: FontStyles.textDark12w500,
+                  ),
+                  transactionType == 'income'
+                      ? Text(
+                          '+ $transactionAmount IDR',
+                          textAlign: TextAlign.left,
+                          style: FontStyles.textPrimary18bold,
+                        )
+                      : Text(
+                          '- $transactionAmount IDR',
+                          textAlign: TextAlign.left,
+                          style: FontStyles.textDanger18bold,
+                        ),
+                  Text(
+                    transactionDate,
+                    textAlign: TextAlign.left,
+                    style: FontStyles.textDark12,
+                  ),
+                ],
+              ),
+            )
+          ],
         ),
       ),
     );
