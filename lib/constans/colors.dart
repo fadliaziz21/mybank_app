65e8d189fed9de7d3988d0e9fbb804af7cd96629
Fadli Abdul Aziz <fadli.aziz@anabatic.com>
2023-07-07 18:05:22 +0700

    add new lightGreyColor

--- lib/constans/colors.dart
+++ lib/constans/colors.dart
@@ -4,6 +4,7 @@ class AppColors {
   static const Color primaryColor = Color.fromARGB(255, 0, 127, 191);
   static const Color dangerColor = Color.fromARGB(255, 248, 68, 79);
   static const Color darkColor = Color.fromARGB(255, 0, 0, 0);
+  static const Color lightGreyColor = Color.fromARGB(255, 189, 189, 189);
   static const Color secondaryColor = Color.fromARGB(255, 66, 66, 66);
   static const Color whiteColor = Color.fromARGB(255, 255, 255, 255);
   static const Color whiteTransparentColor = Color.fromARGB(61, 255, 255, 255);
