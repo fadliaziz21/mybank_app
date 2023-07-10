a48060ee0431dc7aa272dac5fd97f088af7dd4a9
Fadli Abdul Aziz <fadli.aziz@anabatic.com>
2023-07-10 14:17:32 +0700

    remove appbar,_buildAppBar,add ProfilePage

--- lib/features/dashboard/dashboard_screen.dart
+++ lib/features/dashboard/dashboard_screen.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:mybank_app/constans/assets.dart';
 import 'package:mybank_app/constans/colors.dart';
 import 'package:mybank_app/constans/styles.dart';
 import 'package:mybank_app/features/dashboard/dashboard_data.dart';
 import 'package:mybank_app/features/dashboard_home/dashboard_home_screen.dart';
+import 'package:mybank_app/features/profile/profile_screen.dart';
 
 class DashboardPage extends StatefulWidget {
   const DashboardPage({super.key});
@@ -30,10 +30,6 @@ class _DashboardPageState extends State<DashboardPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: PreferredSize(
-        preferredSize: const Size.fromHeight(80.0),
-        child: _buildAppBar(),
-      ),
       body: SafeArea(
         child: _buildBody(),
       ),
@@ -41,39 +37,12 @@ class _DashboardPageState extends State<DashboardPage> {
     );
   }
 
-  Widget _buildAppBar() {
-    return AppBar(
-      automaticallyImplyLeading: false,
-      backgroundColor: AppColors.primaryColor,
-      shadowColor: AppColors.secondaryColor,
-      toolbarHeight: 80.0,
-      title: Container(
-        width: 40.0,
-        child: Image.asset(Assets.myBankIcon),
-      ),
-      actions: [
-        IconButton(
-          icon: const Icon(
-            Icons.notifications,
-            color: AppColors.whiteColor,
-            size: 30.0,
-          ),
-          onPressed: () {},
-        ),
-      ],
-    );
-  }
-
   Widget _buildBody() {
     switch (selectedIndex) {
       case 0:
         return const DashboardHomePage();
-      // case 1:
-      //   return const Center(child: Text('Search'));
-      // case 2:
-      //   return const Center(child: Text('Account'));
       default:
-        return const Center(child: Text('Default'));
+        return const ProfilePage();
     }
   }
 
