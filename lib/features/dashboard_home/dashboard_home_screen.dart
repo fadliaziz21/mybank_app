392c227a29a63a76c7ce75bc8fcae422f628ed81
Fadli Abdul Aziz <fadli.aziz@anabatic.com>
2023-07-10 14:20:24 +0700

    add _buildAppBar func

--- lib/features/dashboard_home/dashboard_home_screen.dart
+++ lib/features/dashboard_home/dashboard_home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mybank_app/constans/assets.dart';
 import 'package:mybank_app/constans/colors.dart';
 import 'package:mybank_app/constans/styles.dart';
 import 'package:mybank_app/features/dashboard_home/dashboard_home_data.dart';
@@ -17,15 +18,46 @@ class _DashboardHomePageState extends State<DashboardHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            homeContainer(),
-            savingsContainer(),
-            recentTransactionTitleContainer(),
-            recentTransactionListContainer()
-          ],
+      appBar: PreferredSize(
+        preferredSize: const Size.fromHeight(80.0),
+        child: _buildAppBar(),
+      ),
+      body: _buildBody(),
+    );
+  }
+
+  Widget _buildAppBar() {
+    return AppBar(
+      automaticallyImplyLeading: false,
+      backgroundColor: AppColors.primaryColor,
+      shadowColor: AppColors.secondaryColor,
+      toolbarHeight: 80.0,
+      title: Container(
+        width: 40.0,
+        child: Image.asset(Assets.myBankIcon),
+      ),
+      actions: [
+        IconButton(
+          icon: const Icon(
+            Icons.notifications,
+            color: AppColors.whiteColor,
+            size: 30.0,
+          ),
+          onPressed: () {},
         ),
+      ],
+    );
+  }
+
+  Widget _buildBody() {
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          homeContainer(),
+          savingsContainer(),
+          recentTransactionTitleContainer(),
+          recentTransactionListContainer()
+        ],
       ),
     );
   }
