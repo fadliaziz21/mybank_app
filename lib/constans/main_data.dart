e1d017ce78039bc3007d58af988c6aa5d7bebcb4
Fadli Abdul Aziz <fadli.aziz@anabatic.com>
2023-07-17 14:09:23 +0700

    add new List Map variable

--- lib/constans/main_data.dart
+++ lib/constans/main_data.dart
@@ -40,8 +40,10 @@ class AppData {
 
   static List<Map<String, dynamic>> accountInformation = [
     {
-      'accountName': 'John Doe',
+      'accountName': 'Roberto Downey Yanuar',
       'accountNumber': '1234567890',
+      'accountStatus': 'Basic',
+      'balance': '9.876.543,18',
       'cardType': {
         'cardImage': cardType[1]['cardImage'],
         'cardName': cardType[1]['cardName'],
