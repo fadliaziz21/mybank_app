584a7f5d87d661404c859d23960748aeb19d81dd
Fadli Abdul Aziz <fadli.aziz@anabatic.com>
2023-08-28 17:37:57 +0700

    add new profileSettingList data

--- lib/features/profile/data/profile_data.dart
+++ lib/features/profile/data/profile_data.dart
@@ -1,3 +1,13 @@
+import 'package:flutter/material.dart';
 import 'package:mybank_app/constans/main_data.dart';
+import 'package:mybank_app/features/profile/models/profile_models.dart';
 
 Map<String, dynamic> accountInfoList = AppData.accountInformation[0];
+
+List profileSettingList = [
+  ProfileSettingItem(Icons.person_sharp, 'My Profile'),
+  ProfileSettingItem(Icons.verified_user, 'Account Security'),
+  ProfileSettingItem(Icons.payment, 'Limit & Payment Settings'),
+  ProfileSettingItem(Icons.settings, 'General Settings'),
+  ProfileSettingItem(Icons.card_giftcard, 'Refer a Friend'),
+];
