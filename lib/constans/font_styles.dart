81d40f5a94d810cdf1a04ab5a6c3a0c59e7b9668
Fadli Abdul Aziz <fadli.aziz@anabatic.com>
2023-08-28 15:42:38 +0700

    separate the Styles Class and FontStyles

---
+++ lib/constans/font_styles.dart
@@ -0,0 +1,117 @@
+import 'package:flutter/material.dart';
+import 'package:mybank_app/constans/colors.dart';
+import 'package:mybank_app/constans/font_family.dart';
+
+class FontStyles {
+  static const TextStyle textPrimary = TextStyle(color: AppColors.primaryColor);
+  static const TextStyle textDark = TextStyle(color: AppColors.darkColor);
+  static const TextStyle textWhite = TextStyle(color: AppColors.whiteColor);
+
+  static const TextStyle textPrimary16bold = TextStyle(
+    fontFamily: FontFamily.montserrat,
+    fontSize: 16,
+    fontWeight: FontWeight.bold,
+    color: AppColors.primaryColor,
+  );
+  static const TextStyle textPrimary18bold = TextStyle(
+    fontFamily: FontFamily.montserrat,
+    fontSize: 18,
+    fontWeight: FontWeight.bold,
+    color: AppColors.primaryColor,
+  );
+  static const TextStyle textPrimary28bold = TextStyle(
+    fontFamily: FontFamily.montserrat,
+    fontSize: 28,
+    fontWeight: FontWeight.bold,
+    color: AppColors.primaryColor,
+  );
+
+  static const TextStyle textDanger18bold = TextStyle(
+    fontFamily: FontFamily.montserrat,
+    fontSize: 18,
+    fontWeight: FontWeight.bold,
+    color: AppColors.dangerColor,
+  );
+
+  static const TextStyle textDark12 = TextStyle(
+    fontFamily: FontFamily.montserrat,
+    fontSize: 12,
+    fontWeight: FontWeight.normal,
+    color: AppColors.darkColor,
+  );
+  static const TextStyle textDark12w500 = TextStyle(
+    fontFamily: FontFamily.montserrat,
+    fontSize: 12,
+    fontWeight: FontWeight.w500,
+    color: AppColors.darkColor,
+  );
+
+  static const TextStyle textDark14 = TextStyle(
+    fontFamily: FontFamily.montserrat,
+    fontSize: 14,
+    fontWeight: FontWeight.normal,
+    color: AppColors.darkColor,
+  );
+  static const TextStyle textDark14w500 = TextStyle(
+    fontFamily: FontFamily.montserrat,
+    fontSize: 14,
+    fontWeight: FontWeight.w500,
+    color: AppColors.darkColor,
+  );
+  static const TextStyle textDark14bold = TextStyle(
+    fontFamily: FontFamily.montserrat,
+    fontSize: 14,
+    fontWeight: FontWeight.bold,
+    color: AppColors.darkColor,
+  );
+  static const TextStyle textDark16 = TextStyle(
+    fontFamily: FontFamily.montserrat,
+    fontSize: 16,
+    fontWeight: FontWeight.normal,
+    color: AppColors.darkColor,
+  );
+  static const TextStyle textDark16bold = TextStyle(
+    fontFamily: FontFamily.montserrat,
+    fontSize: 16,
+    fontWeight: FontWeight.bold,
+    color: AppColors.darkColor,
+  );
+  static const TextStyle textDark18bold = TextStyle(
+    fontFamily: FontFamily.montserrat,
+    fontSize: 18,
+    fontWeight: FontWeight.bold,
+    color: AppColors.darkColor,
+  );
+  static const TextStyle textDark20bold = TextStyle(
+    fontFamily: FontFamily.montserrat,
+    fontSize: 20,
+    fontWeight: FontWeight.bold,
+    color: AppColors.darkColor,
+  );
+
+  static const TextStyle textWhite11bold = TextStyle(
+    fontFamily: FontFamily.montserrat,
+    fontSize: 11,
+    fontWeight: FontWeight.bold,
+    color: AppColors.whiteColor,
+  );
+  static const TextStyle textWhite16 = TextStyle(
+    fontFamily: FontFamily.montserrat,
+    fontSize: 16,
+    fontWeight: FontWeight.normal,
+    color: AppColors.whiteColor,
+  );
+  static const TextStyle textWhite18bold = TextStyle(
+    fontFamily: FontFamily.montserrat,
+    fontSize: 18,
+    fontWeight: FontWeight.bold,
+    color: AppColors.whiteColor,
+  );
+
+  static const TextStyle textLightGrey14 = TextStyle(
+    fontFamily: FontFamily.montserrat,
+    fontSize: 14,
+    fontWeight: FontWeight.normal,
+    color: AppColors.lightGreyColor,
+  );
+}
