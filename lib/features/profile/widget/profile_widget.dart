d501329ade069f1346a5b76af1edc4d8f5be4b20
Fadli Abdul Aziz <fadli.aziz@anabatic.com>
2023-08-28 17:38:38 +0700

    add new ProfileList class widget

--- lib/features/profile/widget/profile_widget.dart
+++ lib/features/profile/widget/profile_widget.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:mybank_app/constans/colors.dart';
 import 'package:mybank_app/constans/styles.dart';
 
+import '../../../constans/font_styles.dart';
+
 class ProfilePicture extends StatelessWidget {
   final String urlImage;
   final String accountStatus;
@@ -74,6 +76,72 @@ class ProfilePicturePremium extends StatelessWidget {
           ),
         )
       ],
+    );
+  }
+}
+
+class ProfileList extends StatelessWidget {
+  final IconData iconName;
+  final String settingName;
+  final int index;
+  final int profileListLength;
+
+  const ProfileList(
+    this.index,
+    this.profileListLength, {
+    super.key,
+    required this.iconName,
+    required this.settingName,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: Styles.eiAll5,
+      decoration: index == 0 && index == profileListLength - 1
+          ? Styles.decorationAll15BorderLightGreyW1
+          : index == 0
+              ? Styles.decorationTop15BorderLightGreyW1
+              : index == profileListLength - 1
+                  ? Styles.decorationBottom15BorderLightGreyW1
+                  : Styles.decorationBorderLightGreyW1,
+      child: Row(
+        children: [
+          Padding(
+            padding: Styles.eiAll5,
+            child: Icon(
+              iconName,
+              color: AppColors.primaryColor,
+              size: 30.0,
+            ),
+          ),
+          Row(
+            children: [
+              Padding(
+                padding: Styles.eiAll7,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Text(
+                      settingName,
+                      textAlign: TextAlign.left,
+                      style: FontStyles.textDark14w500,
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+          const Spacer(),
+          IconButton(
+            icon: const Icon(Icons.arrow_forward_ios),
+            iconSize: 20.0,
+            color: AppColors.lightGreyColor,
+            onPressed: () {},
+          ),
+        ],
+      ),
     );
   }
 }
