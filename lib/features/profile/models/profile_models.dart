c79820e1944a65cdabf9a2c0a3ab26fc52d0a9ad
Fadli Abdul Aziz <fadli.aziz@anabatic.com>
2023-08-28 17:38:14 +0700

    add new ProfileSettingItem models class

---
+++ lib/features/profile/models/profile_models.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class ProfileSettingItem {
+  ProfileSettingItem(this.iconName, this.settingName);
+
+  final IconData iconName;
+  final String settingName;
+}
